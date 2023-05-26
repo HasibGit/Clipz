a5f4e685a32dc7de3544799fd4bb6f67daa2a8c2
Hasib Ullah <hasibullah04@gmail.com>
2023-05-26 15:00:07 +0600

    added a presentational component for input

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,21 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './components/modal/modal.component';
 import { TabsContainerComponent } from './components/tabs-container/tabs-container.component';
 import { TabComponent } from './components/tab/tab.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
-  declarations: [ModalComponent, TabsContainerComponent, TabComponent],
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
   imports: [CommonModule],
-  exports: [ModalComponent, TabsContainerComponent, TabComponent],
+  exports: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
 })
 export class SharedModule {}
