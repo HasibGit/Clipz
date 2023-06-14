717f01cdcc6381a0be50abc05d9bc2bd445626fe
Hasib Ullah <hasibullah04@gmail.com>
2023-06-14 16:25:42 +0600

    angular fire sdk setup done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC9MWFrkYcQGiwVe4ilx6VBaNyTWsB8KUk',
+    authDomain: 'clipz-66d97.firebaseapp.com',
+    projectId: 'clipz-66d97',
+    storageBucket: 'clipz-66d97.appspot.com',
+    messagingSenderId: '430113374325',
+    appId: '1:430113374325:web:30985fd971209d919d783c',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC9MWFrkYcQGiwVe4ilx6VBaNyTWsB8KUk',
+    authDomain: 'clipz-66d97.firebaseapp.com',
+    projectId: 'clipz-66d97',
+    storageBucket: 'clipz-66d97.appspot.com',
+    messagingSenderId: '430113374325',
+    appId: '1:430113374325:web:30985fd971209d919d783c',
+  },
 };
 
 /*
