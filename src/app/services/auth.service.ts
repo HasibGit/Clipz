c93182638979e28f2acdbf2ddde23f08b3c251c2
Hasib Ullah <hasibullah04@gmail.com>
2023-06-16 18:30:18 +0600

    added type safty to firestore collection

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
 import { IUser } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private _afa: AngularFireAuth, private _db: AngularFirestore) {}
+  usersCollection: AngularFirestoreCollection<IUser>;
+
+  constructor(private _afa: AngularFireAuth, private _db: AngularFirestore) {
+    this.usersCollection = this._db.collection('users');
+  }
 
   async createUser(userData: IUser) {
+    if (!userData?.password) {
+      throw new Error('Password not provided');
+    }
+
     // Register user
     await this._afa.createUserWithEmailAndPassword(
       userData.email,
@@ -24,6 +35,6 @@ export class AuthService {
       phoneNumber: userData.phoneNumber,
     };
 
-    await this._db.collection('users').add(newUser);
+    await this.usersCollection.add(newUser);
   }
 }
