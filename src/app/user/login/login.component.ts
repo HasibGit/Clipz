6bec2bf5222ab3688ca78bca21bddeee87c4c643
Hasib Ullah <hasibullah04@gmail.com>
2023-06-23 18:08:23 +0600

    login functionality implemented

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -11,11 +13,38 @@ export class LoginComponent implements OnInit {
     password: '',
   };
 
-  constructor() {}
+  alertColor = 'blue';
+  alertMsg = '';
+  showAlert = false;
+
+  isLoading = false;
+
+  constructor(private _afa: AngularFireAuth) {}
 
   ngOnInit(): void {}
 
-  onSubmit() {
-    console.log(this.credentials);
+  async onSubmit() {
+    this.isLoading = true;
+    this.showAlert = true;
+    this.alertMsg = 'Please wait. Your are being logged into your account.';
+    this.alertColor = 'blue';
+
+    try {
+      await this._afa.signInWithEmailAndPassword(
+        this.credentials.email,
+        this.credentials.password
+      );
+    } catch (e) {
+      console.log(e);
+      this.alertMsg = 'Sorry, something went wrong!';
+      this.alertColor = 'red';
+      this.isLoading = false;
+
+      return;
+    }
+
+    this.alertMsg = 'You have successfully logged into your account.';
+    this.alertColor = 'green';
+    this.isLoading = false;
   }
 }
